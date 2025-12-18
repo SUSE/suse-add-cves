SUSE/suse-add-cves

commit 656329a55e4a8379d05d6530828bc113202559ac

curl: remove
All helpers were moved to slcurl++.

suse-add-cves.cc
@@ -9,11 +9,11 @@
 #include <sys/stat.h>
 #include <utime.h>
 
+#include <sl/curl/Curl.h>
 #include <sl/git/Repo.h>
 
 #include "helpers.h"
 #include "cves.h"
-#include "curl.h"
 #include "cve2bugzilla.h"
 
 namespace {
@@ -43,8 +43,10 @@ int main(int argc, char **argv)
 		fail_with_message("You must provide at least one patch or clone the vulns repository with --init (-i) and --vulns (-v)!  See --help (-h)!");
 
 	constexpr const char cve2bugzilla_url[] = "https://gitlab.suse.de/security/cve-database/-/raw/master/data/cve2bugzilla";
-	auto cve2bugzilla_file = fetch_file_if_needed({}, "cve2bugzilla.txt", cve2bugzilla_url,
-						      false, false, false, std::chrono::hours{12});
+	auto cve2bugzilla_file = SlCurl::LibCurl::fetchFileIfNeeded("cve2bugzilla.txt",
+								    cve2bugzilla_url,
+								    false, false,
+								    std::chrono::hours{12});
 
 	if (gm.init) {
 		if (!gm.vulns.empty()) {
@@ -73,9 +75,9 @@ int main(int argc, char **argv)
 
 			if (mtime < now - expires_after) {
 				SlGit::Repo::update(gm.vulns);
-				cve2bugzilla_file = fetch_file_if_needed({}, "cve2bugzilla.txt",
+				cve2bugzilla_file = SlCurl::LibCurl::fetchFileIfNeeded("cve2bugzilla.txt",
 									 cve2bugzilla_url,
-									 false, true, false,
+									 true, false,
 									 std::chrono::hours{12});
 			}
 			std::filesystem::last_write_time(origin_master_ref, now);

curl.h
@@ -1,1 +1,0 @@
-../curl.h

commit d6c948aa273bc3e1696dfd34324f9e46aa02982c

suse-add-cves: store cve2bugzilla.txt into cache dir
After the commit below, cve2bugzilla.txt was stored to a current
directory. Fix this, so it is stored in the cache dir again.

Fixes: 656329a55e4a ("curl: remove")

suse-add-cves.cc
@@ -195,7 +195,10 @@ int handled_main(int argc, char **argv)
 	if (!cve_hash_map)
 		fail_with_message("Couldn't load kernel vulns database git tree");
 
-	const auto cve2bugzilla_file = SlCurl::LibCurl::fetchFileIfNeeded("cve2bugzilla.txt",
+	auto cacheDir = SlHelpers::HomeDir::createCacheDir("suse-get-maintainers");
+	if (cacheDir.empty())
+		fail_with_message("Unable to create a cache dir");
+	const auto cve2bugzilla_file = SlCurl::LibCurl::fetchFileIfNeeded(cacheDir / "cve2bugzilla.txt",
 									  "https://gitlab.suse.de/security/cve-database/-/raw/master/data/cve2bugzilla",
 									  forceCVE2Bugzilla, false,
 									  std::chrono::hours{12});